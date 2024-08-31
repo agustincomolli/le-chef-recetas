415730b58ab85dc9664e5e3aaf63bdb372442ef6
Agustín Comolli <agustincomolli@gmail.com>
2024-08-31 16:30:12 -0300

    fix: now sqlite

--- app/config.py
+++ app/config.py
@@ -23,22 +23,7 @@ class Config:
 
     """
     database_path = os.path.join(basedir, 'data', 'recipes.db')
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + database_path
-
-    # Configuración de la base de datos
-    DB_USER = os.environ.get('DB_USER', 'postgres')
-    DB_PASS = os.environ.get('DB_PASS', 'postgres')
-    DB_HOST = os.environ.get('DB_HOST', 'localhost')
-    DB_PORT = os.environ.get('DB_PORT', '5432')
-    DB_NAME = os.environ.get('DB_NAME', 'recipes')
-
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
-    # Asegurarse de que la URL comience con 'postgresql://'
-    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace(
-            "postgres://", "postgresql://", 1)
-
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + database_path
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(24)
     SESSION_PERMANENT = False
