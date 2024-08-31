5449c2a965f5fce063a8287bb6a1a63aa77e161c
Agustín Comolli <agustincomolli@gmail.com>
2024-08-31 16:21:01 -0300

    fix: postgres config

--- app/config.py
+++ app/config.py
@@ -26,16 +26,11 @@ class Config:
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + database_path
 
     # Configuración de la base de datos
-    DB_USER = os.environ.get('PGUSER', 'postgres')
-    DB_PASS = os.environ.get('PGPASSWORD', 'postgres')
-    DB_HOST = os.environ.get('PGHOST', 'localhost')
-    DB_PORT = os.environ.get('PGPORT', '5432')
-    DB_NAME = os.environ.get('PGDATABASE', 'recipes')
-    # DB_USER = os.environ.get('DB_USER', 'postgres')
-    # DB_PASS = os.environ.get('DB_PASS', 'postgres')
-    # DB_HOST = os.environ.get('DB_HOST', 'localhost')
-    # DB_PORT = os.environ.get('DB_PORT', '5432')
-    # DB_NAME = os.environ.get('DB_NAME', 'recipes')
+    DB_USER = os.environ.get('DB_USER', 'postgres')
+    DB_PASS = os.environ.get('DB_PASS', 'postgres')
+    DB_HOST = os.environ.get('DB_HOST', 'localhost')
+    DB_PORT = os.environ.get('DB_PORT', '5432')
+    DB_NAME = os.environ.get('DB_NAME', 'recipes')
 
     SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
